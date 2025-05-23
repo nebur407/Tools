efd83153e25d9213f0e9fbe41e36f83447b6971e
nebur407 <rubenkps04@gmail.com>
2025-05-24 04:45:55 +1000

    Add files via upload

--- Macchanger.py
+++ Macchanger.py
@@ -1,44 +1,109 @@
+print("""
+     4    000   77777
+    44   0   0     77
+   4 4   0   0    77
+  44444  0   0   77
+     4   0   0  77
+     4    000  77
+
+******************************
+*  realizado por nebur407    *
+******************************
+
+""")
 import subprocess
-import optparse
+import argparse
 import re
+import random
+import time
+import platform
+def detect_system(interface):
+    system = platform.system()
+    try:
+        if system == "Linux" or system == "Darwin":
+            subprocess.check_output(["ifconfig", interface])
+        elif system == "Windows":
+            print("[-] This script is not supported on Windows.")
+            exit()
+        else:
+            print(f"[-] Unsupported system: {system}")
+            exit()
+    except Exception as e:
+        print(f"[-] Could not check interface: {e}")
+        exit()
 
 def get_arguments():
-    parser = optparse.OptParseError
+    parser = argparse.ArgumentParser(description="Tool to change MAC address")
 
-    parser.add_option("-i", "--interface", dest="interface", help="Interface para cambiar Dirrecion MAC")
-    parser.add_option("-m", "--mac", dest="interface", help="Nueva dirrecion MAC")
-    parser.add_option("-b", "--interface", dest="interface", help="Cambio infinito de MAC")
-    (options, arguments)=parser.parse_args()
-    if not options.interface:
-        parser.error("[-] Por favor indica una Interfaz, usa --help para mas informacion")
-    elif not options.new_mac:
-        parser.error("[-] Por favor indica una Nueva dirrecion MAC, usa --help para mas informacion")
-    return options
+    parser.add_argument("-i", "--interface", dest="interface", required=True, help="Interface to change the MAC address")
+    parser.add_argument("-m", "--mac", dest="new_mac", help="New MAC address (optional)")
+    parser.add_argument("-b", "--infinite", dest="infinite", action="store_true", help="Change MAC in infinite loop press Crtl + C to end.")
+    parser.add_argument("-t", "--time", dest="loop_time", type=int, help="Interval time (in seconds) for infinite MAC changes press Crtl + C to end.")
 
-def cambiar_MAC(interface, new_mac):
-    print(f"[+] Cambiar dirrecion MAC para  {interface}  a  {new_mac}")
+    return parser.parse_args()
 
-    subprocess.call(f"ifconfig {interface} down " , shell=True)
+def set_mac(interface, new_mac):
+    print(f"[+] Changing MAC address for {interface} to {new_mac}")
+    subprocess.call(f"ifconfig {interface} down", shell=True)
     subprocess.call(f"ifconfig {interface} hw ether {new_mac}", shell=True)
-    subprocess.call(f"ifconfig {interface} up ", shell=True)
-def get_MAC(interface):
-    ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-    print(ifconfig_result)
-    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    subprocess.call(f"ifconfig {interface} up", shell=True)
 
-    if mac_address_search_result:
-        print(mac_address_search_result.group(0))
-    else:
-        print("[-] La dirrecion no sigue los estandares de una MAC")
+def get_mac(interface):
+    try:
+        output = subprocess.check_output(["ifconfig", interface]).decode("utf-8")
+        mac_search = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", output)
+        if mac_search:
+            return mac_search.group(0)
+        else:
+            print("[-] MAC address not found or invalid format.")
+            return None
+    except Exception as e:
+        print(f"[-] Error getting MAC address: {e}")
+        return None
+
+def generate_random_mac():
+    mac = [0x00, 0x16, 0x3e,
+           random.randint(0x00, 0x7f),
+           random.randint(0x00, 0xff),
+           random.randint(0x00, 0xff)]
+    return ":".join(f"{x:02x}" for x in mac)
+
+def change_mac_loop(interface, delay):
+    try:    
+        while True:
+            mac = generate_random_mac()
+            print(f"[+] New random MAC: {mac}")
+            set_mac(interface, mac)
+            time.sleep(delay)
+    except KeyboardInterrupt:
+        print("[!] Infinite loop ended")
+
+def change_mac_infinite(interface):
+    try:    
+        while True:
+            mac = generate_random_mac()
+            print(f"[+] New random MAC: {mac}")
+            set_mac(interface, mac)
+    except KeyboardInterrupt:
+        print("[!] Infinite loop ended")
+    
 
 options = get_arguments()
-current_MAC = get_MAC(options.interface)
-print(f"Current MAC = {current_MAC}")
 
-cambiar_MAC(options.interface, options.new_mac)
+detect_system(options.interface)
 
-current_MAC = get_MAC(options.interface)
-if current_MAC == options.new_mac:
-    print("[+] MAC cambio correctamente a {current_MAC}")
+original_mac = get_mac(options.interface)
+print(f"[+] Current MAC: {original_mac}")
+
+if options.infinite and options.loop_time:
+    change_mac_loop(options.interface, options.loop_time)
+elif options.infinite:
+    change_mac_infinite(options.interface)
+elif options.new_mac:
+    set_mac(options.interface, options.new_mac)
+
+updated_mac = get_mac(options.interface)
+if updated_mac == options.new_mac:
+    print(f"[+] MAC successfully changed to {updated_mac}")
 else:
-    print("[-] MAC no ha sido cambiada")
+    print(f"[!] Final MAC: {updated_mac}")
